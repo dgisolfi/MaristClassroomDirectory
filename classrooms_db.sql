284c77efeb8c98eda52f9b63dd5a374bd724a94b
dgisolfi <daniel.gisolfi1@marist.edu>
2017-12-17 13:28:51 -0500

    now shows all available software and a image of the setup

--- classrooms_db.sql
+++ classrooms_db.sql
@@ -3,46 +3,51 @@
 #Authors: Daniel Gisolfi
 #Version 0.1
 
-#Create the database
-CREATE DATABASE IF NOT EXISTS classrooms_db;
-USE classrooms_db;
+-- #Create the database
+-- CREATE DATABASE IF NOT EXISTS classrooms_db;
+-- USE classrooms_db;
 
--- #Create the user table
-CREATE TABLE IF NOT EXISTS admins(
-	user_id 		INT 	AUTO_INCREMENT 	PRIMARY KEY		NOT NULL,
-	first_name 		TEXT,
-	last_name 		TEXT,
-	user_name		TEXT,
-	email 			TEXT,
-	pass 			TEXT);
+-- -- #Create the user table
+-- CREATE TABLE IF NOT EXISTS admins(
+-- 	user_id 		INT 	AUTO_INCREMENT 	PRIMARY KEY		NOT NULL,
+-- 	first_name 		TEXT,
+-- 	last_name 		TEXT,
+-- 	user_name		TEXT,
+-- 	email 			TEXT,
+-- 	pass 			TEXT);
 
 
--- #Populate the user table
-INSERT INTO admins(first_name, last_name, user_name, email, pass)
-VALUES("Daniel", "Gisolfi", "stdg1", "Daniel.Gisolfi1@marist.edu", "dan");
+-- -- #Populate the user table
+-- INSERT INTO admins(first_name, last_name, user_name, email, pass)
+-- VALUES("Daniel", "Gisolfi", "stdg1", "Daniel.Gisolfi1@marist.edu", "dan");
 
--- #Create the buildings table
-CREATE TABLE IF NOT EXISTS buildings(
-	build_id 		INT 	 AUTO_INCREMENT 	PRIMARY KEY		NOT NULL,
-	building_Name	TEXT);
+-- -- #Create the buildings table
+-- CREATE TABLE IF NOT EXISTS buildings(
+-- 	build_id 		INT 	 AUTO_INCREMENT 	PRIMARY KEY		NOT NULL,
+-- 	building_Name	TEXT);
 
-INSERT INTO buildings(building_Name)
-VALUES("Cannavino Library"),
-		("Donnelly Hall"),
-		("Dyson Center"),
-		("Fontaine  Hall"),
-		("Hancock Center"),
-		("Lowell Thomas Communications Center"),
-		("McCann Center"),
-		("Science and Allied Health Building"),
-		("Steel Plant Studios and Gallery"),
-		("Student Center/Music Building");
+-- INSERT INTO buildings(building_Name)
+-- VALUES("Cannavino Library"),
+-- 		("Donnelly Hall"),
+-- 		("Dyson Center"),
+-- 		("Fontaine  Hall"),
+-- 		("Hancock Center"),
+-- 		("Lowell Thomas Communications Center"),
+-- 		("McCann Center"),
+-- 		("Science and Allied Health Building"),
+-- 		("Steel Plant Studios and Gallery"),
+-- 		("Student Center/Music Building");
 
 
 CREATE TABLE IF NOT EXISTS rooms(
 	room_id			INT 	 AUTO_INCREMENT 	PRIMARY KEY 		NOT NULL,
 	room_type		SET('Lab', 'Classroom')		NOT NULL,
 	room_num		INT 						NOT NULL,
+	room_tel		INT,
+	room_pod_ctrl 	SET('Crestron', 'Extron'),
+	-- room_pod_comp	TEXT,
+	room_pod_sft	TEXT,
+	room_pod_img	TEXT,
 	fk_build_id 	INT  						NOT NULL);
 
 
@@ -51,11 +56,11 @@ ALTER TABLE rooms
 ADD FOREIGN KEY (fk_build_id) REFERENCES buildings(build_id);
 
 
-INSERT INTO rooms(room_type, room_num, fk_build_id)
-VALUES("Classroom", 301, 1),
-		("Classroom", 303, 1),
-		("Lab", 333, 1),
-		("Lab", 110, 1);
+INSERT INTO rooms(room_type, room_num, room_tel, room_pod_ctrl, room_pod_sft, room_pod_img, fk_build_id)
+VALUES("Classroom", 	301, 3472, "Crestron", "/pages/software.txt", "/pages/logo.png", 1),
+		("Classroom", 	303, 3349, "Extron","/pages/software.txt", "/pages/logo.png", 1),
+		("Lab", 		333, 3247, "Crestron", "/pages/software.txt", "/pages/logo.png", 1),
+		("Lab", 		110, 2398, "Extron", "/pages/software.txt", "/pages/logo.png",  1);
 
 
 
--- scripts/show_record.php
+++ scripts/show_record.php
@@ -4,7 +4,7 @@ $debug = true;
 
 function show_record($dbc, $id, $room_num) {
   # Create a query to get the name and price sorted by price
-  $query = "SELECT room_type, room_num FROM rooms WHERE fk_build_id = '". $id . "' AND room_num = '" . $room_num . "'";
+  $query = "SELECT * FROM rooms WHERE fk_build_id = '". $id . "' AND room_num = '" . $room_num . "'";
 
   # Execute the query
   $results = mysqli_query($dbc, $query);
@@ -13,10 +13,17 @@ function show_record($dbc, $id, $room_num) {
   # Generate item information
   if($results){
     if($row = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
+      echo '<p>Building: ' . buildingToName($id) . '</p>';
       echo '<p>Room Type: ' . $row['room_type'] . '</p>';
       echo '<p>Room Number: ' . $row['room_num'] . '</p>';
-      echo '<p>Building: ' . buildingToName($id) . '</p>';
-            
+      echo '<p>Phone Number: '. $row['room_tel'] .'</p>';
+      // echo '<p>Podium Computer Details: '. $row['room_pod_comp'] .'</p>';
+      echo '<p>Podium Controller type: '. $row['room_pod_ctrl'] .'</p>';
+      echo '<p>Available Software:</p>';
+      echo '<object data='. $row['room_pod_sft'] .'></object>';
+      echo '<p>Current Podium Setup:</p>';
+      echo '<img src="'.$row['room_pod_img'].'>"';
+       echo '<p><img src=/pages/logo.png><p>';
     }
       # Free up the results in memory
       mysqli_free_result( $results ) ;
