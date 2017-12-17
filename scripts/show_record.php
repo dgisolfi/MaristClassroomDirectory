0ad9ea373e3bdc5334d5ddb5bb79b992f5a2c5a4
dgisolfi <daniel.gisolfi1@marist.edu>
2017-12-17 13:39:55 -0500

    fixed douible image issue

--- scripts/show_record.php
+++ scripts/show_record.php
@@ -22,8 +22,7 @@ function show_record($dbc, $id, $room_num) {
       echo '<p>Available Software:</p>';
       echo '<object data='. $row['room_pod_sft'] .'></object>';
       echo '<p>Current Podium Setup:</p>';
-      echo '<img src="'.$row['room_pod_img'].'>"';
-       echo '<p><img src=/pages/logo.png><p>';
+      echo '<img src='.$row['room_pod_img'].'>';
     }
       # Free up the results in memory
       mysqli_free_result( $results ) ;
